50c6815607a50662aa912d207ab8fe783f91eff8
OriginalShrapnel <flimsyytaco@gmail.com>
2025-03-04 23:46:18 -0600

    Commit for monobot.h

---
+++ Monobot/include/monobot.h
@@ -0,0 +1,10 @@
+#ifndef MONOBOT_H
+#define MONOBOT_H
+
+// Global definitions and includes
+#include <windows.h>
+
+// Function prototypes (we'll add more later)
+DWORD WINAPI MainThread(LPVOID lpParam);
+
+#endif
